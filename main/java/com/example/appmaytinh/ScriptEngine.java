eb9abb4ad1f780d09c69c0500f678ddabbf46d3d
Tandu-png <tandu1739@gmail.com>
2025-05-20 21:53:36 +0700

    Delete main directory

--- main/java/com/example/appmaytinh/ScriptEngine.java
+++
@@ -1,7 +0,0 @@
-package com.example.appmaytinh;
-
-public class ScriptEngine {
-    public Object eval(String expr) {
-        return null;
-    }
-}
--- main/java/com/example/appmaytinh/ScriptEngineManager.java
+++
@@ -1,7 +0,0 @@
-package com.example.appmaytinh;
-
-public class ScriptEngineManager {
-    public ScriptEngine getEngineByName(String rhino) {
-        return null;
-    }
-}
